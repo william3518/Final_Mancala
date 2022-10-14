c52f47affb38e05ca8be5a41b18fb718d581c887
rgoeas <rgoeas@potomacschool.org>
2022-10-14 09:07:45 -0400

    Commit 1 Game class Game class v2

--- Gameboard_Class.py
+++ Gameboard_Class.py
@@ -7,21 +7,47 @@ class Gameboard:
             print(self.board[i])
 
     def move_pieces(self, start):
-        self.row = 1
-        self.piece_num = self.board[self.row][start]
-        self.board[self.row][start] = 0
+        row = 1
+        piece_num = self.board[row][start]
+        self.board[row][start] = 0
         start += 1
-        while self.piece_num > 0:
-            if self.row == 1 and start <= 5:
-                self.board[self.row][start] += 1
+        while piece_num > 0:
+            if row == 1 and start <= 5:
+                self.board[row][start] += 1
                 start += 1
-            elif self.row == 1 and start == 6:
-                self.board[self.row][start] += 1
-                self.row -= 1
-            elif self.row == 0 and start >= 1:
-                self.board[self.row][start] += 1
+            elif row == 1 and start == 6:
+                self.board[row][start] += 1
+                row -= 1
+            elif row == 0 and start >= 1:
+                self.board[row][start] += 1
                 start -= 1
             else:
-                self.board[self.row][start] += 1
-                self.row += 1
-            self.piece_num -= 1
+                self.board[row][start] += 1
+                row += 1
+            piece_num -= 1
+            last_piece = [row, start]
+            return last_piece
+
+    def extra_turn(self, end):
+        if end == [1, 6]:
+            return True
+        else:
+            return False
+
+    def capture(self, end):
+        if end[0] == 1:
+            if self.board[end[0]][end[1]] == 1:
+                if self.board[0][end[1]] > 0:
+                    capture_sum = self.board[end[0]][end[1]] + self.board[0][end[1]]
+                    self.board[1][6] += capture_sum
+                    self.board[end[0]][end[1]] = 0
+                    self.board[0][end[1]] = 0
+
+    def flip_board(self):
+        temp = self.board[1]
+        self.board[1] = self.board[0]
+        self.board[0] = temp
+        self.board[0] = self.board[0].reverse()
+        self.board[1] = self.board[1].reverse()
+
+
