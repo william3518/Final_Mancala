53b584dda6c8b4350054e34bd3548b48275ecc2b
willhart <whart@potomacschool.org>
2022-10-18 09:33:27 -0400

    v7

--- Main.py
+++ Main.py
@@ -6,12 +6,12 @@ from Gameboard_Class import Gameboard
 p1 = Human(1, "Will")
 p2 = Human(2, "Robert")
 gb1 = Gameboard([1, 6])
-
-
 g1 = Game(p1, p2, gb1)
+starting_turn = 1
 
-while g1.turn(1):
-    print()
+while g1.turn(starting_turn):
+   if g1.gb.end_game() == True:
+       break
 
 
 
