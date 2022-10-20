cd7016ab12eb94ef61eba99e0331b7beeedf6812
willhart <whart@potomacschool.org>
2022-10-20 09:31:36 -0400

    v7

--- Game_Class.py
+++ Game_Class.py
@@ -4,15 +4,22 @@ from Gameboard_Class import Gameboard
 
 
 class Game:
-    def __init__(self, p1, p2, gb):
+    def __init__(self, p1, p2, gb, current_player):
         self.p1 = p1
         self.p2 = p2
         self.gb = gb
+        self.current_player = current_player
 
     def start_point(self):
         print("")
         print("[1, 2, 3, 4, 5, 6]")
-        pocket_num = int(input("What pocket do you want to start from? (1-6): "))
+        pocket_num = 7
+        while 6 < pocket_num or pocket_num < 1:
+            pocket_num = int(input("What pocket do you want to start from? (1-6): "))
+            if 6 >= pocket_num > 0:
+                break
+            else:
+                continue
         return pocket_num
 
     def end_turn(self, current_player):
@@ -36,6 +43,7 @@ class Game:
         self.gb.capture()
         self.gb.flip_board()
         self.end_turn(current_player)
+
 
 
 
